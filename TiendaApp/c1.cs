2c720073cc13aad9ad9698184c55e5583c6cba66
Sebastián Martínez Pérez <smartinez@pitasoft.com>
2026-03-22 12:50:57 +0100

    Initialize TiendaApp project with basic structure and .NET 8 settings

---
+++ TiendaApp/c1.cs
@@ -0,0 +1,48 @@
+﻿namespace TiendaApp;
+
+public class c1 
+{
+    public int t; 
+    public int a; 
+        
+    // Calcula el total
+    // Devuelve True si va bien
+    public decimal Calc(decimal p, int q) 
+    {
+        // Declaro r y le asigno p por q
+        decimal r = p * q; 
+            
+        if (p > 0 && q > 0)
+        {
+            if (t == 1)
+            {
+                r = r * 0.90m;
+            }
+            else if (t == 2)
+            {
+                r = r * 0.80m;
+                    
+                if (a > 5)
+                {
+                    r = r - 5; 
+                }
+            }
+        }
+        else
+        {
+            throw new Exception("Error");
+        }
+            
+        return r;
+    }
+
+    public void ImprimirFactura(decimal p, int q)
+    {
+        Console.WriteLine("***************************");
+        Console.WriteLine("**** FACTURA DE COMPRA ****");
+        Console.WriteLine("***************************");
+        decimal total = Calc(p, q);
+        Console.WriteLine("Total a pagar: " + total + " EUR");
+        Console.WriteLine("Gracias por su visita.");
+    }
+}
